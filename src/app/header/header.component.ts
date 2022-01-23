c0f31df252c63e80715019a8e7e09c24fe6f396b
sunil pc <goyalsunil452@gmail.com>
2022-01-23 20:58:10 +0530

    .

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,7 @@
 import { Component, HostListener, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { fromEvent, Subject } from 'rxjs';
 import { NgModule } from '@angular/core';
-// window.onscroll = function() {myFunction()};
 
-// var header = document.getElementById("navbar");
-// if(header!=null){
-//   var sticky = header.offsetTop;
-// }
-
-// function myFunction() {
-//   if (window.pageYOffset > sticky && header!=null) {
-//     console.log(header);
-//     header.classList.add("sticky");
-//   } else if(window.pageYOffset < sticky && header!=null){
-//     console.log(header);
-//     header.classList.remove("sticky");
-//   }
-// }
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -32,7 +17,6 @@ export class HeaderComponent implements OnInit,OnDestroy {
   ngOnInit(): void {
     console.log(this.scrollY);
     window.onscroll = () => {
-      console.log(window.scrollY)
       if(window.scrollY>20){
         this.scrollY=false;
       }else{
